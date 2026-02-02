7534fcdfa415ee5261a779242148cad00c3b96ce
iahub360-stack <iahub360@gmail.com>
2026-02-02 06:20:04 -0300

    Debug Divers

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import asyncio
 import sys
-from aiogram import Bot, Dispatcher
+
+from aiogram import Bot
+from aiogram.dispatcher.dispatcher import Dispatcher
+
 from config import TOKEN
 from handlers import start, shopping, bank, darkmarket, darklabs
+
 
 async def main():
     print(f"--- INICIANDO DARK IRIS HALL ---")
     print(f"Python Version: {sys.version}")
 
-import asyncio
-
-
-async def main():
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
-    # Incluindo as rotas (handlers) de cada seção do Shopping
     dp.include_routers(
         start.router,
         shopping.router,
@@ -26,6 +25,7 @@ async def main():
 
     print("Iris Hall Bot Online...")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
